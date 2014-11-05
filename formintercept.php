38f7ed09a3626b63c57f7966042b1fe159427982
Abigail Liskey <abigail3306@gmail.com>
2014-11-05 18:33:30 -0500

    Uploaded Project

---
+++ formintercept.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Thank you for filling out our form!  We'll get back with you shortly!</title>
+    <link href="structure.css" type="text/css" rel="stylesheet" />
+    <link href="default.css" type="text/css" rel="stylesheet" />
+   	<!--Page Created by Abigail Liskey-->
+	<!--Blue Ridge Community College Fall 2010 ITD110 Professor Oliver-->
+</head>
+<body>
+  <div id="container">
+
+	<div id="header">
+		<h1>Travel Naboo</h1>
+		<h3>Official Naboo Travel Site</h3>
+	</div>
+
+	<div id="menuContainer">
+	<div id="menu">
+		<ul>
+		<li class="borderLeft"><h2>Home</h2>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+			</ul>
+		</li>
+		</ul>
+
+		<ul>
+			<li><h2>About Naboo</h2>
+			<ul>
+				<li><a href="generalinfo.html">General Information</a></li>
+				<li><a href="history.html">History of Naboo</a></li>
+			</ul>
+			</li>
+		</ul>
+
+		<ul>
+			<li><h2>Destinations</h2>
+			<ul>
+    			<li><a href="theed.html" title="Theed">Theed</a></li>
+    			<li><a href="otohgunga.html" title="Otoh Gunga">Otoh Gunga</a></li>		
+			</ul>
+			</li>
+		</ul>
+
+		<ul>
+  			<li><h2>Attractions</h2>
+  			<ul>
+  				<li><a href="lianormswamp.html" title="Lianorm Swamp">Lianorm Swamp</a></li>	
+  				<li><a href="lakecountry.html" title="Lake Country">Lake Country</a></li>
+  			</ul>
+  			</li>
+		</ul>
+
+		<ul>
+			<li><h2>Plan Your Trip</h2>
+			<ul>
+				<li><a href="plantrip.html">Plan Your Trip</a></li>
+			</ul>
+		</li>
+		</ul>
+
+		<ul>
+			<li><h2>Contact</h2>
+			<ul>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+			</li>
+		</ul>	
+		
+		<div>
+		<img src="images/mainlogo.jpg" alt="Naboo" />
+		</div>			
+	</div>
+	</div>
+
+	
+	<div id="contentContainer">
+	<div id="content" class="contentBg">
+		<h2>Travel Naboo - Majestic and Mysterious</h2>
+		<h4>Come and visit Naboo, the planet full of beautiful vast landscapes, extraordinary creatures, and a history rich of heritage!</h4>
+
+		<div class="contentInside">
+			    <?php echo("<h3>Thank you for your information. We'll get back to you shortly!</h3>"); ?>
+			    
+		</div>
+		
+	</div>
+	</div>
+
+	<div id="sidebar2Container">
+	<div id="sidebar2">
+		<h3>Travel News</h3>
+		<div class="contentInside">
+			<p>Tours through the <a href="lianormswamp.html">Lianorm Swamp</a> will currently be postponed until further notice due to aggressive animal attacks.  We are sorry for the inconvenience.</p>
+		</div>
+		<div class="contentInside">
+			<p>Theed's grand opening of the toy droid gift shop.  Come and buy your droid replicas today!</p>
+		</div>
+			<h4>To stay updated, join our email list and sign up for our quarterly newsletter...!</h4>
+	</div>
+	</div>
+	
+</div>
+
+	<div id="footer">
+		<p>Travel Naboo - Majestic and Mysterious. 34,000 light years away from the Deep Core. &copy;Copyright</p>
+	</div>   
+ 
+</body>
+</html>
